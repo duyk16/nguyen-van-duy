0e5f3619c0c6d616dc60e60aed446dfeb52c8719
David Nguyen <duyk16@gmail.com>
2024-07-19 22:48:37 +0700

    feat: :tada: Initialized problem 5 project structure

---
+++ src/problem5/src/app.ts
@@ -0,0 +1,48 @@
+import compression from 'compression';
+import cors from 'cors';
+import express, { Express } from 'express';
+import helmet from 'helmet';
+import httpStatus from 'http-status';
+import config from './config/config';
+import { ApiError, errorConverter, errorHandler } from './modules/errors';
+import { morgan } from './modules/logger';
+import routes from './routes/v1';
+
+const app: Express = express();
+
+if (config.env !== 'test') {
+  app.use(morgan.successHandler);
+  app.use(morgan.errorHandler);
+}
+
+// set security HTTP headers
+app.use(helmet());
+
+// enable cors
+app.use(cors());
+app.options('*', cors());
+
+// parse json request body
+app.use(express.json());
+
+// parse urlencoded request body
+app.use(express.urlencoded({ extended: true }));
+
+// gzip compression
+app.use(compression());
+
+// v1 api routes
+app.use('/v1', routes);
+
+// send back a 404 error for any unknown api request
+app.use((req, res, next) => {
+  next(new ApiError(httpStatus.NOT_FOUND, 'Not found'));
+});
+
+// convert error to ApiError, if needed
+app.use(errorConverter);
+
+// handle error
+app.use(errorHandler);
+
+export default app;
